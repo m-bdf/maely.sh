4e3e81befba57977c0230ada64d2cce2e5616e6b
Maëlys Bras de fer <mae.bdf@outlook.com>
2023-12-15 02:07:10 +0100

    Svelte website base

--- flake.nix
+++ flake.nix
@@ -7,9 +7,17 @@
       {
         formatter = pkgs.nixpkgs-fmt;
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs.elmPackages;
-            [ elm elm-format elm-analyse elm-live ];
+        devShells = {
+          website = pkgs.mkShell {
+            packages = with pkgs; [ nodejs_latest bun ];
+            shellHook = "cd website";
+          };
+
+          resume = pkgs.mkShell {
+            packages = with pkgs.elmPackages;
+              [ elm elm-format elm-analyse elm-live ];
+            shellHook = "cd resume";
+          };
         };
       }
     );
